dce552ce60f2f7cefe934ed49c55a0c19a4670a6
Raine Soriano <rainearcher@g.ucla.edu>
2023-12-10 02:33:48 -0800

    Read current sleep power policies

--- NoSleep.cs
+++ NoSleep.cs
@@ -1,11 +1,85 @@
 using System;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
 
+
+public class WindowsPowerCFG
+{
+    public int get_plugged_in_sleep_after_seconds()
+    {
+        return get_sleep_after_seconds(PowerReadACValue);
+    }
+    public int get_on_battery_sleep_after_seconds()
+    {
+        return get_sleep_after_seconds(PowerReadDCValue);
+    }
+
+    private int get_sleep_after_seconds(PowerReadValueFunction readValueFunction)
+    {
+        var activePolicyGuidPTR = IntPtr.Zero;
+        PowerGetActiveScheme(IntPtr.Zero, ref activePolicyGuidPTR);
+
+        var activePolicyGuid = Marshal.PtrToStructure<Guid>(activePolicyGuidPTR);
+        var type = 0;
+        var value = 0;
+        var valueSize = 4u;
+        readValueFunction(IntPtr.Zero, ref activePolicyGuid,
+            ref GUID_SLEEP_SUBGROUP, ref GUID_STANDBYIDLE,
+            ref type, ref value, ref valueSize);
+
+        return value;
+    }
+
+    [DllImport("powrprof.dll")]
+    static extern uint PowerGetActiveScheme(
+        IntPtr UserRootPowerKey,
+        ref IntPtr ActivePolicyGuid);
+
+    private delegate uint PowerReadValueFunction(
+        IntPtr RootPowerKey,
+        ref Guid SchemeGuid,
+        ref Guid SubGroupOfPowerSettingGuid,
+        ref Guid PowerSettingGuid,
+        ref int Type,
+        ref int Buffer,
+        ref uint BufferSize);
+
+    [DllImport("powrprof.dll")]
+    static extern uint PowerReadACValue(
+        IntPtr RootPowerKey,
+        ref Guid SchemeGuid,
+        ref Guid SubGroupOfPowerSettingGuid,
+        ref Guid PowerSettingGuid,
+        ref int Type,
+        ref int Buffer,
+        ref uint BufferSize);
+
+    [DllImport("powrprof.dll")]
+    static extern uint PowerReadDCValue(
+        IntPtr RootPowerKey,
+        ref Guid SchemeGuid,
+        ref Guid SubGroupOfPowerSettingGuid,
+        ref Guid PowerSettingGuid,
+        ref int Type,
+        ref int Buffer,
+        ref uint BufferSize);
+
+    private static Guid GUID_SLEEP_SUBGROUP =
+        new Guid("238c9fa8-0aad-41ed-83f4-97be242c8f20");
+    private static Guid GUID_STANDBYIDLE =
+        new Guid("29f6c1db-86da-48c5-9fdb-f2b67b1f44da");
+
+}
 
 public class NoSleep
 {
     static void Main()
     {
+        var powerCfg = new WindowsPowerCFG();
+        Console.WriteLine("started NoSleep");
+        Console.WriteLine($"when plugged in pc will sleep after {powerCfg.get_plugged_in_sleep_after_seconds()} seconds");
+        Console.Write($"on battery power pc will sleep after {powerCfg.get_on_battery_sleep_after_seconds()} seconds");
         new NoSleep();
         Application.Run();
     }
