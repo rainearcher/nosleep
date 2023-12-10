3229b3d524771b8437a867f24287b597b92257da
Raine Soriano <rainearcher@g.ucla.edu>
2023-12-10 01:35:06 -0800

    refactoring

--- NoSleep.cs
+++ NoSleep.cs
@@ -6,28 +6,48 @@ public class NoSleep
 {
     static void Main()
     {
-        var icon = new NoSleep();
+        new NoSleep();
         Application.Run();
     }
 
     public NoSleep()
     {
-        notifyIcon = new NotifyIcon();
-        contextMenu = new ContextMenuStrip();
-        menuItem = new ToolStripMenuItem();
+        init_items_in_order();
+    }
 
+    private void init_items_in_order()
+    {
+        init_menu_item();
+        init_context_menu();
+        init_notify_icon();
+    }
+
+    private void init_menu_item()
+    {
+        menuItem = new ToolStripMenuItem {
+            Text = "E&xit"
+        };
+        menuItem.Click += new EventHandler(menu_item_click);
+
+    }
+
+    private void init_context_menu()
+    {
+        contextMenu = new ContextMenuStrip();
         contextMenu.Items.AddRange(
             new ToolStripMenuItem[] {menuItem});
 
-        menuItem.Text = "E&xit";
-        menuItem.Click += new EventHandler(this.menu_item_click);
+    }
 
-        notifyIcon.Icon = new System.Drawing.Icon("images/moon.ico");
-
-        notifyIcon.ContextMenuStrip = contextMenu;
-
-        notifyIcon.Text = "NoSleep";
-        notifyIcon.Visible = true;
+    private void init_notify_icon()
+    {
+        notifyIcon = new NotifyIcon
+        {
+            Icon = new System.Drawing.Icon("images/moon.ico"),
+            ContextMenuStrip = contextMenu,
+            Text = "NoSleep",
+            Visible = true
+        };
     }
 
     private void menu_item_click(object Sender, EventArgs e)
