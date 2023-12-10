138a296b5b1aa50da165b379ff6cdf73bddba27a
Raine Soriano <rainearcher@g.ucla.edu>
2023-12-10 03:39:48 -0800

    Refactor powercfg into new file

--- NoSleep.cs
+++ NoSleep.cs
@@ -1,171 +1,16 @@
 using System;
-using System.Runtime.InteropServices;
 using System.Windows.Forms;
-using Microsoft.VisualBasic;
 
-
-public class WindowsPowerCFG
-{
-
-    public WindowsPowerCFG()
-    {
-        userPluggedInSleepAfterSeconds = get_plugged_in_sleep_after_seconds();
-        userOnBatterySleepAfterSeconds = get_on_battery_sleep_after_seconds();
-    }
-
-    public void disable_sleep()
-    {
-        set_on_battery_sleep_after_seconds(0);
-        set_plugged_in_sleep_after_seconds(0);
-    }
-
-    public void reenable_sleep()
-    {
-        set_on_battery_sleep_after_seconds(userOnBatterySleepAfterSeconds);
-        set_plugged_in_sleep_after_seconds(userPluggedInSleepAfterSeconds);
-    }
-
-    public void debug_read_powercfg()
-    {
-        Console.WriteLine($"when plugged in pc will sleep after {userPluggedInSleepAfterSeconds} seconds");
-        Console.WriteLine($"on battery power pc will sleep after {userOnBatterySleepAfterSeconds} seconds");
-        set_plugged_in_sleep_after_seconds(0);
-        set_on_battery_sleep_after_seconds(240);
-        Console.WriteLine($"when plugged in pc will sleep after {get_plugged_in_sleep_after_seconds()} seconds");
-        Console.WriteLine($"on battery power pc will sleep after {get_on_battery_sleep_after_seconds()} seconds");
-        set_plugged_in_sleep_after_seconds(userOnBatterySleepAfterSeconds);
-        set_on_battery_sleep_after_seconds(userOnBatterySleepAfterSeconds);
-    }
-
-    private int get_plugged_in_sleep_after_seconds()
-    {
-        return get_sleep_after_seconds(PowerReadACValue);
-    }
-    private int get_on_battery_sleep_after_seconds()
-    {
-        return get_sleep_after_seconds(PowerReadDCValue);
-    }
-
-    private void set_plugged_in_sleep_after_seconds(int seconds)
-    {
-        set_sleep_after_seconds(seconds, PowerWriteACValueIndex);
-    }
-
-    private void set_on_battery_sleep_after_seconds(int seconds)
-    {
-        set_sleep_after_seconds(seconds, PowerWriteDCValueIndex);
-    }
-
-    private int get_sleep_after_seconds(PowerReadValueFunction readValueFunction)
-    {
-        var activePolicyGuid = get_active_scheme_guid();
-        var type = 0;
-        var value = 0;
-        var valueSize = 4u;
-        readValueFunction(IntPtr.Zero, ref activePolicyGuid,
-            ref GUID_SLEEP_SUBGROUP, ref GUID_STANDBYIDLE,
-            ref type, ref value, ref valueSize);
-
-        return value;
-    }
-
-    private void set_sleep_after_seconds(int seconds, PowerWriteValueIndexFunction powerWriteValue)
-    {
-        var activePolicyGuid = get_active_scheme_guid();
-        powerWriteValue(
-            IntPtr.Zero,
-            ref activePolicyGuid,
-            ref GUID_SLEEP_SUBGROUP,
-            ref GUID_STANDBYIDLE,
-            seconds);
-    }
-
-    private Guid get_active_scheme_guid()
-    {
-        var activePolicyGuidPTR = IntPtr.Zero;
-        PowerGetActiveScheme(IntPtr.Zero, ref activePolicyGuidPTR);
-
-        return Marshal.PtrToStructure<Guid>(activePolicyGuidPTR);
-
-    }
-
-    private int userPluggedInSleepAfterSeconds;
-    private int userOnBatterySleepAfterSeconds;
-
-    [DllImport("powrprof.dll")]
-    static extern uint PowerGetActiveScheme(
-        IntPtr UserRootPowerKey,
-        ref IntPtr ActivePolicyGuid);
-
-    private delegate uint PowerReadValueFunction(
-        IntPtr RootPowerKey,
-        ref Guid SchemeGuid,
-        ref Guid SubGroupOfPowerSettingGuid,
-        ref Guid PowerSettingGuid,
-        ref int Type,
-        ref int Buffer,
-        ref uint BufferSize);
-
-    [DllImport("powrprof.dll")]
-    static extern uint PowerReadACValue(
-        IntPtr RootPowerKey,
-        ref Guid SchemeGuid,
-        ref Guid SubGroupOfPowerSettingGuid,
-        ref Guid PowerSettingGuid,
-        ref int Type,
-        ref int Buffer,
-        ref uint BufferSize);
-
-    [DllImport("powrprof.dll")]
-    static extern uint PowerReadDCValue(
-        IntPtr RootPowerKey,
-        ref Guid SchemeGuid,
-        ref Guid SubGroupOfPowerSettingGuid,
-        ref Guid PowerSettingGuid,
-        ref int Type,
-        ref int Buffer,
-        ref uint BufferSize);
-
-    private delegate uint PowerWriteValueIndexFunction(
-        IntPtr RootPowerKey,
-        ref Guid SchemeGuid,
-        ref Guid SubGroupOfPowerSettingsGuid,
-        ref Guid PowerSettingGuid,
-        int AcValueIndex);
-
-    [DllImport("powrprof.dll")]
-    static extern uint PowerWriteACValueIndex(
-        IntPtr RootPowerKey,
-        ref Guid SchemeGuid,
-        ref Guid SubGroupOfPowerSettingsGuid,
-        ref Guid PowerSettingGuid,
-        int AcValueIndex);
-
-    [DllImport("powrprof.dll")]
-    static extern uint PowerWriteDCValueIndex(
-        IntPtr RootPowerKey,
-        ref Guid SchemeGuid,
-        ref Guid SubGroupOfPowerSettingsGuid,
-        ref Guid PowerSettingGuid,
-        int AcValueIndex);
-
-    private static Guid GUID_SLEEP_SUBGROUP =
-        new Guid("238c9fa8-0aad-41ed-83f4-97be242c8f20");
-    private static Guid GUID_STANDBYIDLE =
-        new Guid("29f6c1db-86da-48c5-9fdb-f2b67b1f44da");
-
-}
-
-public class NoSleep
+public class NoSleepIcon
 {
     static void Main()
     {
         Console.WriteLine("started NoSleep");
-        var noSleep = new NoSleep();
+        var noSleep = new NoSleepIcon();
         Application.Run();
     }
 
-    public NoSleep()
+    public NoSleepIcon()
     {
         init_exit_menu_item();
         init_sleep_on_off_sleep_menu_item();
@@ -213,7 +58,7 @@ public class NoSleep
 
     private void init_powercfg()
     {
-        powerCfg = new WindowsPowerCFG();
+        powerCfg = new WindowsPowercfgConnector();
         powerCfg.debug_read_powercfg();
     }
 
@@ -243,7 +88,7 @@ public class NoSleep
         }
     }
 
-    private WindowsPowerCFG powerCfg;
+    private WindowsPowercfgConnector powerCfg;
     private NotifyIcon notifyIcon;
     private ContextMenuStrip contextMenu;
     private ToolStripMenuItem sleepOnOffMenuItem;
---
+++ PowerCFGConnector.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+public class WindowsPowercfgConnector
+{
+    public WindowsPowercfgConnector()
+    {
+        userPluggedInSleepAfterSeconds = get_plugged_in_sleep_after_seconds();
+        userOnBatterySleepAfterSeconds = get_on_battery_sleep_after_seconds();
+    }
+
+    public void disable_sleep()
+    {
+        set_on_battery_sleep_after_seconds(0);
+        set_plugged_in_sleep_after_seconds(0);
+    }
+
+    public void reenable_sleep()
+    {
+        set_on_battery_sleep_after_seconds(userOnBatterySleepAfterSeconds);
+        set_plugged_in_sleep_after_seconds(userPluggedInSleepAfterSeconds);
+    }
+
+    public void debug_read_powercfg()
+    {
+        Console.WriteLine($"when plugged in pc will sleep after {userPluggedInSleepAfterSeconds} seconds");
+        Console.WriteLine($"on battery power pc will sleep after {userOnBatterySleepAfterSeconds} seconds");
+        set_plugged_in_sleep_after_seconds(0);
+        set_on_battery_sleep_after_seconds(240);
+        Console.WriteLine($"when plugged in pc will sleep after {get_plugged_in_sleep_after_seconds()} seconds");
+        Console.WriteLine($"on battery power pc will sleep after {get_on_battery_sleep_after_seconds()} seconds");
+        set_plugged_in_sleep_after_seconds(userOnBatterySleepAfterSeconds);
+        set_on_battery_sleep_after_seconds(userOnBatterySleepAfterSeconds);
+    }
+
+    private int get_plugged_in_sleep_after_seconds()
+    {
+        return get_sleep_after_seconds(PowerReadACValue);
+    }
+    private int get_on_battery_sleep_after_seconds()
+    {
+        return get_sleep_after_seconds(PowerReadDCValue);
+    }
+
+    private void set_plugged_in_sleep_after_seconds(int seconds)
+    {
+        set_sleep_after_seconds(seconds, PowerWriteACValueIndex);
+    }
+
+    private void set_on_battery_sleep_after_seconds(int seconds)
+    {
+        set_sleep_after_seconds(seconds, PowerWriteDCValueIndex);
+    }
+
+    private int get_sleep_after_seconds(PowerReadValueFunction readValueFunction)
+    {
+        var activePolicyGuid = get_active_scheme_guid();
+        var type = 0;
+        var value = 0;
+        var valueSize = 4u;
+        readValueFunction(IntPtr.Zero, ref activePolicyGuid,
+            ref GUID_SLEEP_SUBGROUP, ref GUID_STANDBYIDLE,
+            ref type, ref value, ref valueSize);
+
+        return value;
+    }
+
+    private void set_sleep_after_seconds(int seconds, PowerWriteValueIndexFunction powerWriteValue)
+    {
+        var activePolicyGuid = get_active_scheme_guid();
+        powerWriteValue(
+            IntPtr.Zero,
+            ref activePolicyGuid,
+            ref GUID_SLEEP_SUBGROUP,
+            ref GUID_STANDBYIDLE,
+            seconds);
+    }
+
+    private Guid get_active_scheme_guid()
+    {
+        var activePolicyGuidPTR = IntPtr.Zero;
+        PowerGetActiveScheme(IntPtr.Zero, ref activePolicyGuidPTR);
+
+        return Marshal.PtrToStructure<Guid>(activePolicyGuidPTR);
+
+    }
+
+    private int userPluggedInSleepAfterSeconds;
+    private int userOnBatterySleepAfterSeconds;
+
+    [DllImport("powrprof.dll")]
+    static extern uint PowerGetActiveScheme(
+        IntPtr UserRootPowerKey,
+        ref IntPtr ActivePolicyGuid);
+
+    private delegate uint PowerReadValueFunction(
+        IntPtr RootPowerKey,
+        ref Guid SchemeGuid,
+        ref Guid SubGroupOfPowerSettingGuid,
+        ref Guid PowerSettingGuid,
+        ref int Type,
+        ref int Buffer,
+        ref uint BufferSize);
+
+    [DllImport("powrprof.dll")]
+    static extern uint PowerReadACValue(
+        IntPtr RootPowerKey,
+        ref Guid SchemeGuid,
+        ref Guid SubGroupOfPowerSettingGuid,
+        ref Guid PowerSettingGuid,
+        ref int Type,
+        ref int Buffer,
+        ref uint BufferSize);
+
+    [DllImport("powrprof.dll")]
+    static extern uint PowerReadDCValue(
+        IntPtr RootPowerKey,
+        ref Guid SchemeGuid,
+        ref Guid SubGroupOfPowerSettingGuid,
+        ref Guid PowerSettingGuid,
+        ref int Type,
+        ref int Buffer,
+        ref uint BufferSize);
+
+    private delegate uint PowerWriteValueIndexFunction(
+        IntPtr RootPowerKey,
+        ref Guid SchemeGuid,
+        ref Guid SubGroupOfPowerSettingsGuid,
+        ref Guid PowerSettingGuid,
+        int AcValueIndex);
+
+    [DllImport("powrprof.dll")]
+    static extern uint PowerWriteACValueIndex(
+        IntPtr RootPowerKey,
+        ref Guid SchemeGuid,
+        ref Guid SubGroupOfPowerSettingsGuid,
+        ref Guid PowerSettingGuid,
+        int AcValueIndex);
+
+    [DllImport("powrprof.dll")]
+    static extern uint PowerWriteDCValueIndex(
+        IntPtr RootPowerKey,
+        ref Guid SchemeGuid,
+        ref Guid SubGroupOfPowerSettingsGuid,
+        ref Guid PowerSettingGuid,
+        int AcValueIndex);
+
+    private static Guid GUID_SLEEP_SUBGROUP =
+        new Guid("238c9fa8-0aad-41ed-83f4-97be242c8f20");
+    private static Guid GUID_STANDBYIDLE =
+        new Guid("29f6c1db-86da-48c5-9fdb-f2b67b1f44da");
+
+}
